1fb3c9755f480b32abbc70ae4d535127e6b53ebb
Praharsh Singh <praharsh535@gmail.com>
2021-04-24 13:32:20 +0530

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://quiet-fjord-73073.herokuapp.com/",
 });
 const authLink = setContext(() => {
   const token = localStorage.getItem("jwtToken");
